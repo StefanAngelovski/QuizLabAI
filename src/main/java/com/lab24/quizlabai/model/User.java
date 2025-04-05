6af33a3404fd91db56d763a5deaace1cdab5c610
stanchevskid <dejan.silver@gmail.com>
2025-04-05 19:31:37 +0200

    Create authentication models

--- src/main/java/com/lab24/quizlabai/model/User.java
+++ src/main/java/com/lab24/quizlabai/model/User.java
@@ -2,13 +2,20 @@ package com.lab24.quizlabai.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Setter
 @Getter
 @Entity
+@NoArgsConstructor
 @Table(name = "user", schema = "public")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,5 +28,13 @@ public class User {
 
     @Column(nullable = false, unique = true)
     private String email;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(role);
+    }
 
 }
