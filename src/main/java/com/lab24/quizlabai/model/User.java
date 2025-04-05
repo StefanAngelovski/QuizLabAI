c0aa046f7a61387ad4f084fc479670aa4fb2879d
stanchevskid <dejan.silver@gmail.com>
2025-04-05 19:26:16 +0200

    Fix database connection

--- src/main/java/com/lab24/quizlabai/model/User.java
+++ src/main/java/com/lab24/quizlabai/model/User.java
@@ -1,9 +1,13 @@
 package com.lab24.quizlabai.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
-@Table(name = "users")
+@Table(name = "user", schema = "public")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,37 +22,4 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
