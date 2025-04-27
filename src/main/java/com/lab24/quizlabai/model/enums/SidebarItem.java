47be58507dd150c74ba70d235c306ab8a53c3b7d
stanchevskid <dejan.silver@gmail.com>
2025-04-27 03:16:56 +0200

    Improve sidebar navigation

--- src/main/java/com/lab24/quizlabai/model/enums/SidebarItem.java
+++ src/main/java/com/lab24/quizlabai/model/enums/SidebarItem.java
@@ -7,28 +7,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public enum SidebarItem {
-    MY_GRADES("My Grades", "🎓", Role.ROLE_STUDENT),
-    GRADES("Grades", "📊", Role.ROLE_PROFESSOR),
-    AVAILABLE_QUIZZES("Available Quizzes", "📝", Role.ROLE_STUDENT),
-    CREATE_QUIZ("Create Quiz", "📝", Role.ROLE_PROFESSOR),
-    SUBMITTED_QUIZZES("Submitted Quizzes", "📥️", Role.ROLE_PROFESSOR),
-    MANAGE_QUIZZES("Manage Quizzes", "✏️", Role.ROLE_PROFESSOR),
-    COMPLETED_QUIZZES("Completed Quizzes", "✅", Role.ROLE_STUDENT),
-    PROFESSOR_FEEDBACK("Professor Feedback", "💬", Role.ROLE_STUDENT),
-    STUDY_RESOURCES("Study Resources", "📚", Role.ROLE_STUDENT, Role.ROLE_STUDENT),
-    REPORTS("Reports & Analytics", "📈", Role.ROLE_PROFESSOR),
-    STUDENTS("Students", "👥", Role.ROLE_PROFESSOR),
-    STATISTICS("Statistics", "🧮", Role.ROLE_PROFESSOR),
-    EDIT_QUIZ("Edit Quiz", "📝", Role.ROLE_PROFESSOR),
-    SETTINGS("Settings", "⚙️", Role.ROLE_PROFESSOR, Role.ROLE_STUDENT);
+    DASHBOARD("Dashboard", "🎛️", "/dashboard", Role.ROLE_STUDENT, Role.ROLE_PROFESSOR, Role.ROLE_ADMIN),
+    MY_GRADES("My Grades", "🎓", "/dashboard", Role.ROLE_STUDENT),
+    GRADES("Grades", "📊", "/dashboard", Role.ROLE_PROFESSOR),
+    AVAILABLE_QUIZZES("Available Quizzes", "📝", "/dashboard", Role.ROLE_STUDENT),
+    CREATE_QUIZ("Create Quiz", "📝", "/quizgeneration", Role.ROLE_PROFESSOR),
+    SUBMITTED_QUIZZES("Submitted Quizzes", "📥️", "/dashboard", Role.ROLE_PROFESSOR),
+    MANAGE_QUIZZES("Manage Quizzes", "✏️", "/quiz-management", Role.ROLE_PROFESSOR),
+    COMPLETED_QUIZZES("Completed Quizzes", "✅", "/dashboard", Role.ROLE_STUDENT),
+    PROFESSOR_FEEDBACK("Professor Feedback", "💬", "/dashboard", Role.ROLE_STUDENT),
+    STUDY_RESOURCES("Study Resources", "📚", "/dashboard", Role.ROLE_STUDENT, Role.ROLE_STUDENT),
+    REPORTS("Reports & Analytics", "📈", "/dashboard", Role.ROLE_PROFESSOR),
+    STUDENTS("Students", "👥", "/dashboard", Role.ROLE_PROFESSOR),
+    STATISTICS("Statistics", "🧮", "/dashboard", Role.ROLE_PROFESSOR),
+    SETTINGS("Settings", "⚙️", "/profile", Role.ROLE_PROFESSOR, Role.ROLE_STUDENT);
 
     private final String title;
     private final String emoji;
+    private final String endpoint;
     private final Role[] roles;
 
-    SidebarItem(String title, String emoji, Role... roles) {
+    SidebarItem(String title, String emoji, String endpoint, Role... roles) {
         this.title = title;
         this.emoji = emoji;
+        this.endpoint = endpoint;
         this.roles = roles;
     }
 
@@ -57,5 +59,9 @@ public enum SidebarItem {
 
     public String getEmoji() {
         return this.emoji;
+    }
+
+    public String getEndpoint() {
+        return this.endpoint;
     }
 }
