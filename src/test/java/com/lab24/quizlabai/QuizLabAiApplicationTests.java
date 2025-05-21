c84e1eaf03a25adb883a248ceebdb17b81dccb94
stanchevskid <dejan.silver@gmail.com>
2025-05-22 01:13:15 +0200

    Test fixes

--- src/test/java/com/lab24/quizlabai/QuizLabAiApplicationTests.java
+++ src/test/java/com/lab24/quizlabai/QuizLabAiApplicationTests.java
@@ -2,12 +2,13 @@ package com.lab24.quizlabai;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class QuizLabAiApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
